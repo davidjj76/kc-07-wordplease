2c4151aaaa5238249f296f738da8585abe88a5ed
davidjj76 <davidjj76@gmail.com>
2017-05-07 20:18:44 +0200

    Creo la vista del detalle de un post

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from blogs.models import Post, Blog
 
@@ -7,16 +7,22 @@ from blogs.models import Post, Blog
 class PostList(ListView):
 
     template_name = 'blogs/latest_posts.html'
-    queryset = Post.objects.published()
+    queryset = Post.objects.published().select_related('blog__owner')
+
+
+class PostDetail(DetailView):
+
+    model = Post
+    template_name = 'blogs/post_detail.html'
 
 
 class BlogList(ListView):
 
     template_name = 'blogs/blog_list.html'
-    queryset = Blog.objects.annotate(num_posts=Count('posts'))
+    queryset = Blog.objects.annotate(num_posts=Count('posts')).select_related('owner')
 
 
-class BlogDetail(PostList, ListView):
+class BlogDetail(PostList):
 
     template_name = 'blogs/blog_detail.html'
 
--- wordplease/urls.py
+++ wordplease/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from blogs.views import PostList, BlogList, BlogDetail
+from blogs.views import PostList, BlogList, BlogDetail, PostDetail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', PostList.as_view(), name='latest_posts'),
     url(r'^blogs/?$', BlogList.as_view(), name='blog_list'),
     url(r'^blogs/(?P<username>[a-zA-Z0-9_]+)/?$', BlogDetail.as_view(), name="blog_detail"),
-    # url(r'^blogs/(?P<username>[a-zA-Z0-9_]+)/(?P<pk>[0-9]+)/?$', PostDetail.as_view(), name="post_detail"),
+    url(r'^blogs/(?P<username>[a-zA-Z0-9_]+)/(?P<pk>[0-9]+)/?$', PostDetail.as_view(), name="post_detail"),
 ]
