948ecb57bc0d10f20b92efb1631c8451c583c168
davidjj76 <davidjj76@gmail.com>
2017-05-07 19:42:11 +0200

    Cambio en las plantillas los nombres de los arrays al generico object_list, y así no tengo que cambiar el nombre del context_object_name

--- blogs/views.py
+++ blogs/views.py
@@ -7,13 +7,11 @@ from blogs.models import Post, Blog
 class PostList(ListView):
 
     template_name = 'blogs/latest_posts.html'
-    context_object_name = 'latest_posts'
     queryset = Post.objects.published()
 
 
 class BlogList(ListView):
 
-    model = Blog
     template_name = 'blogs/blog_list.html'
     queryset = Blog.objects.annotate(num_posts=Count('posts'))
 
@@ -21,7 +19,6 @@ class BlogList(ListView):
 class BlogDetail(PostList, ListView):
 
     template_name = 'blogs/blog_detail.html'
-    context_object_name = 'posts'
 
     def get_queryset(self):
         return super().queryset.filter(blog__owner__username=self.kwargs.get('username'))
