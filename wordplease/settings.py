3c37229605ec016e8067c05f3171e56c52bfa2a5
davidjj76 <davidjj76@gmail.com>
2017-05-08 23:49:51 +0200

    Añado el resto de plantillas y los estáticos

--- wordplease/settings.py
+++ wordplease/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'wordplease',
     'blogs.apps.BlogsConfig',
     'django_extensions'
 ]
