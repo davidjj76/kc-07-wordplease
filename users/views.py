29c5503d680def521a70bc13304a6ca407299810
davidjj76 <davidjj76@gmail.com>
2017-05-09 04:02:46 +0200

    Implemento las vistas de login y logout

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView as DjangoLoginView, LogoutView as DjangoLogoutView
 from django.urls import reverse
 from django.views.generic import FormView
 
@@ -15,3 +16,17 @@ class SignupView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+class LoginView(DjangoLoginView):
+
+    template_name = 'users/login.html'
+
+    def get_success_url(self):
+        return self.request.GET.get('next', reverse('latest_posts'))
+
+
+class LogoutView(DjangoLogoutView):
+
+    def get_next_page(self):
+        return reverse('users_login')
